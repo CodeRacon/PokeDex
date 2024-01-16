063ba344e871d4f503877863077d53049fa3caad
CodeRacon <michael.buschmann.86@gmail.com>
2024-01-16 23:27:27 +0100

    added isLiked - parameter

--- fetch.js
+++ fetch.js
@@ -1,93 +1,167 @@
-async function fetchPokeData(identifier) {
-  let urlOne;
-  let urlTwo;
-
+//
+let baseDataURL;
+let detailDataURL;
+//
+async function fetchAllData(identifier) {
   if (Number.isInteger(identifier)) {
-    // Wenn identifier eine Zahl ist, verwenden Sie es als id
-    urlOne = `https://pokeapi.co/api/v2/pokemon/${identifier}`;
-    urlTwo = `https://pokeapi.co/api/v2/pokemon-species/${identifier}`;
+    baseDataURL = `https://pokeapi.co/api/v2/pokemon/${identifier}`;
+    detailDataURL = `https://pokeapi.co/api/v2/pokemon-species/${identifier}`;
   } else {
-    // Andernfalls nehmen Sie es als Namen
-    urlOne = `https://pokeapi.co/api/v2/pokemon/${identifier.toLowerCase()}`;
-    urlTwo = `https://pokeapi.co/api/v2/pokemon-species/${identifier.toLowerCase()}`;
+    baseDataURL = `https://pokeapi.co/api/v2/pokemon/${identifier.toLowerCase()}`;
+    detailDataURL = `https://pokeapi.co/api/v2/pokemon-species/${identifier.toLowerCase()}`;
   }
-  let respOne = await fetch(urlOne);
-  let fetchedData = await respOne.json();
-  let ID = fetchedData['id'];
-  let pokeID = fetchedData['id'].toString().padStart(4, '0');
-  let pokeName = capFirstLetter(fetchedData['species']['name']);
-  let pokeHeight = fetchedData['height'] / 10; // in meters
-  let pokeWeight = fetchedData['weight'] / 10; // in kilogramm
-  let pokeTypes = fetchedData['types'].map((type) =>
-    capFirstLetter(type.type.name)
-  );
-  let pokeAbilities = fetchedData['abilities'].map((ability) =>
-    capFirstLetter(ability.ability.name)
-  );
-  let pokeSprite =
-    fetchedData['sprites']['other']['official-artwork']['front_default'];
 
-  // stats:
-  let statHP = fetchedData['stats'][0].base_stat;
-  let statAttack = fetchedData['stats'][1].base_stat;
-  let statDefense = fetchedData['stats'][2].base_stat;
-  let statSpAttack = fetchedData['stats'][3].base_stat;
-  let statSpDefense = fetchedData['stats'][4].base_stat;
-  let statSpeed = fetchedData['stats'][5].base_stat;
-  let statTotal =
+  await fetchBaseData(baseDataURL, identifier);
+  await fetchDetailData(detailDataURL, identifier);
+
+  let baseData = await fetchBaseData(baseDataURL);
+  let detailData = await fetchDetailData(detailDataURL);
+
+  let currentPokemon = createPokemonObject(
+    collectBaseData(baseData),
+    collectStats(baseData),
+    await collectDetailData(detailData)
+  );
+
+  pokemonData.push(currentPokemon);
+}
+
+async function fetchBaseData(baseDataURL) {
+  let response = await fetch(baseDataURL);
+  return await response.json();
+}
+
+async function fetchDetailData(detailDataURL) {
+  let response = await fetch(detailDataURL);
+  return await response.json();
+}
+
+function createPokemonObject(baseData, detailData) {
+  collectBaseData(baseData);
+  collectStats(baseData);
+  collectDetailData(detailData);
+}
+
+function collectBaseData(baseData) {
+  return {
+    shortID: baseData['id'],
+    longID: baseData['id'].toString().padStart(4, '0'),
+    capName: capFirstLetter(baseData['species']['name']),
+    height: baseData['height'] / 10, // in meters
+    weight: baseData['weight'] / 10, // in kilogramm
+    sprite: baseData['sprites']['other']['official-artwork']['front_default'],
+    types: baseData['types'].map((type) => capFirstLetter(type.type.name)),
+    abilities: baseData['abilities'].map((ability) =>
+      capFirstLetter(ability.ability.name)
+    ),
+  };
+}
+
+function collectStats(baseData) {
+  const statHP = baseData['stats'][0].base_stat;
+  const statAttack = baseData['stats'][1].base_stat;
+  const statDefense = baseData['stats'][2].base_stat;
+  const statSpAttack = baseData['stats'][3].base_stat;
+  const statSpDefense = baseData['stats'][4].base_stat;
+  const statSpeed = baseData['stats'][5].base_stat;
+  const statTotal =
     statHP +
     statAttack +
     statDefense +
     statSpAttack +
     statSpDefense +
     statSpeed;
-
-  let bestMoves = fetchedData['moves']
+  const bestMoves = baseData['moves']
     .map((move) => capTwoWords(move.move.name))
     .slice(0, 15);
 
-  // --------------------------------------------------------------
-
-  // let urlTwo = `https://pokeapi.co/api/v2/pokemon-species/${id}`;
-
-  let respTwo = await fetch(urlTwo);
-  let fetchedDetail = await respTwo.json();
-
-  let pokeGenus = fetchedDetail['genera'][7].genus;
-  let pokeAbout = fetchedDetail['flavor_text_entries'][1].flavor_text;
-
-  // const evoChainUrl = fetchedDetail.evolution_chain.url;
-  // const evoChainResponse = await fetch(evoChainUrl);
-  // const evoChainData = await evoChainResponse.json();
-
-  // const evoChain = [];
-
-  // extractEvoNames(evoChainData.chain, evoChain);
-
-  // console.log(fetchedDetail['evolution_chain'].url);
-
-  let currentPokemon = {
-    ID: ID,
-    id: pokeID,
-    name: pokeName,
-    types: pokeTypes,
-    abilities: pokeAbilities,
-    sprite: pokeSprite,
-    species: pokeGenus,
-    about: pokeAbout,
-    height: pokeHeight,
-    weight: pokeWeight,
-    // stats:
-    hp: statHP,
-    attack: statAttack,
-    defense: statDefense,
-    sp_attack: statSpAttack,
-    sp_defense: statSpDefense,
-    speed: statSpeed,
-    total: statTotal,
-    moves: bestMoves,
+  return {
+    statHP,
+    statAttack,
+    statDefense,
+    statSpAttack,
+    statSpDefense,
+    statSpeed,
+    statTotal,
+    bestMoves,
   };
-  pokemonData.push(currentPokemon);
+}
+
+async function collectDetailData(detailData) {
+  let genus = detailData['genera'][7].genus;
+  let about = detailData['flavor_text_entries'][1].flavor_text;
+
+  const evoChainUrl = detailData.evolution_chain.url;
+  const evoChainResponse = await fetch(evoChainUrl);
+  const evoChainData = await evoChainResponse.json();
+  const evoChain = [];
+
+  extractStageNames(evoChainData.chain, evoChain);
+
+  const shortStageIDs = await getChainIDs(evoChain);
+
+  return {
+    genus,
+    about,
+    evolution: evoChain,
+    shortStageIDs,
+  };
+}
+
+function createPokemonObject(
+  collectedBaseData,
+  collectedStats,
+  collectedDetailData
+) {
+  return {
+    id: collectedBaseData.shortID,
+    ID: collectedBaseData.longID,
+    name: collectedBaseData.capName,
+    types: collectedBaseData.types,
+    abilities: collectedBaseData.abilities,
+    sprite: collectedBaseData.sprite,
+    species: collectedDetailData.genus,
+    about: collectedDetailData.about,
+    height: collectedBaseData.height,
+    weight: collectedBaseData.weight,
+    hp: collectedStats.statHP,
+    attack: collectedStats.statAttack,
+    defense: collectedStats.statDefense,
+    sp_attack: collectedStats.statSpAttack,
+    sp_defense: collectedStats.statSpDefense,
+    speed: collectedStats.statSpeed,
+    total: collectedStats.statTotal,
+    moves: collectedStats.bestMoves,
+    evolution: collectedDetailData.evolution,
+    shortStageIDs: collectedDetailData.shortStageIDs,
+    isLiked: 'false',
+  };
+}
+
+function extractStageNames(chain, evoChain) {
+  if (chain && chain.species && chain.species.name) {
+    evoChain.push(capFirstLetter(chain.species.name));
+
+    if (chain.evolves_to && chain.evolves_to.length > 0) {
+      chain.evolves_to.forEach((evolution) => {
+        extractStageNames(evolution, evoChain); // Hier wurde das zweite Argument hinzugefügt
+      });
+    }
+  }
+}
+
+async function getChainIDs(evoChain) {
+  const shortStageIDs = [];
+
+  for (const evoElement of evoChain) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${evoElement.toLowerCase()}`;
+    const resp = await fetch(url);
+    const fetchedData = await resp.json();
+    const id = fetchedData['id'];
+    shortStageIDs.push(id);
+  }
+
+  return shortStageIDs;
 }
 
 function capFirstLetter(word) {
